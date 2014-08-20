df3d9e69822b41a11aad7ba495502dfa4bdd9cfc
Ripmax <siranderson66@gmail.com>
2014-08-20 21:25:31 +0100

    Added boot messages

--- lua/autorun/sv_whitelist.lua
+++ lua/autorun/sv_whitelist.lua
@@ -1,5 +1,9 @@
 if(CLIENT) then return end
 
+Msg("// LOADING SERVER WHITELIST ADDON \\\n")
+
 include("whitelist/core.lua")
 include("whitelist/chatcmd.lua")
 include("whitelist/config.lua")
+
+Msg("// LOADED \\\n")
