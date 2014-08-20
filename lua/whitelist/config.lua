156a38258879d3d89d9a6cef4624f545b308bd6a
Ripmax <siranderson66@gmail.com>
2014-08-20 16:35:25 +0100

    Starting on framework.

---
+++ lua/whitelist/config.lua
@@ -0,0 +1,11 @@
+Whitelist.Config = {}
+
+Whitelist.Config["kick"] = false // Set to true to swap out using the CheckPassword hook for kicking the player. (Recommended: false)
+
+Whitelist.Config["useadminmod"] = false // Set to true to use the installed admin mod to kick them if kick is enabled.
+
+Whitelist.Config["disconnectmessage"] = "You're not whitelisted on this server!"
+
+Whitelist.Config["minecraftstyle"] = false // Use minecraft style whitelisting commands (WILL OVERRIDE CHAT COMMAND PREFIX WITH /)
+
+Whitelist.Config["chatcmdprefix"] = "/" // Set chat command prefix.
