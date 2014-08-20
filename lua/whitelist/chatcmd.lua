76c49e5ef9456a03c118a0022edbad95835dff34
Ripmax <siranderson66@gmail.com>
2014-08-20 21:18:31 +0100

    More functionality

--- lua/whitelist/chatcmd.lua
+++ lua/whitelist/chatcmd.lua
@@ -2,13 +2,32 @@ local string = string
 local hook = hook
 local table = table
 local config = Whitelist.Config
+local meta = FindMetaTable("Player")
 
-local function Parse(mg, pl, tm)
-	local text = mg
-	local prefix = string.sub(text, 1, 1)
-	local pref = (config["minecraftstyle"] or "/" and config["chatprefix"])
+local function meta:HasPermission()
+	local Player = self
+	if(!Player || !IsValid(Player)) then return false end
 	
-	if(prefix != pref) then return end
-	
-	local cmd = string.sub(text, 2)
+	if(config["usegroups"]) then
+		return table.HasValue(config["groups"],Player:GetUserGroup())
+	else
+		return (config["superadmin"] and Player:IsSuperAdmin() or Player:IsAdmin())
+	end
 end
+
+local function Parse(text)
+	local prefix = string.sub(text, 1, 1)
+	local pref = (config["minecraftstyle"] and "/" or config["chatprefix"])
+	if(prefix != pref) then return "" end
+	local cmd = string.sub(text, 2)
+	
+	return cmd
+end
+
+hook.Add("PlayerSay",function(Player, text, t)
+	if(!IsValid(Player)) then return false end
+	
+	if(Player:HasPermission()) then
+		local s = Parse(text)
+	end
+end)
--- lua/whitelist/config.lua
+++ lua/whitelist/config.lua
@@ -1,7 +1,14 @@
 Whitelist.Config = {}
 
+Whitelist.Config["usegroups"] = false // Set to true to use groups rather than permission checks. (NOT ALL ADMIN MODS WILL SUPPORT THIS!) (ULX Does)
+
+Whitelist.Config["groups"] = {"admin","superadmin","owner"} // Change these to the groups you want to have access, must have usegroups set to true.
+
+Whitelist.Config["superadmin"] = true // Set to false to allow normal administrators to whitelist players. Will be ignored if usegroups is set to true.
+
 Whitelist.Config["disconnectmessage"] = "You're not whitelisted on this server!"
 
 Whitelist.Config["chatprefix"] = "/" // Set chat command prefix.
 
 Whitelist.Config["minecraftstyle"] = false // Use minecraft style whitelisting commands (WILL OVERRIDE CHAT COMMAND PREFIX WITH /)
+
