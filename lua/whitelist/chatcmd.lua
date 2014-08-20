a7dea937f9c5c1c0146c78b0cd74593c38b60c5c
Ripmax <siranderson66@gmail.com>
2014-08-20 17:09:19 +0100

    Cleanup

--- lua/whitelist/chatcmd.lua
+++ lua/whitelist/chatcmd.lua
@@ -6,16 +6,8 @@ local __regex = "^.(%S+)"
 
 local function Parse(mg, pl, tm)
 	local text = mg
-	local prefix = text:sub(1, 1)
+	local prefix = string.sub(text, 1, 1)
 	local pref = (config["minecraftstyle"] or "/" and config["chatprefix"])
 	
-	if(prefix == pref) then return end
-	
-	local comm
-	text = text:gsub(__regex, function(m)
-		comm = m
-		return ""
-	end, 1)
-	
-	if(!comm || !text || comm == text) then return "" end
+	if(prefix != pref) then return end
 end
