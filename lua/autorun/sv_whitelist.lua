5e2029f46f9bedbf7b158e4c57749d20b5bff960
Ripmax <siranderson66@gmail.com>
2014-08-26 00:11:09 +0100

    Inclusion Update

--- lua/autorun/sv_whitelist.lua
+++ lua/autorun/sv_whitelist.lua
@@ -3,7 +3,9 @@ if(CLIENT) then return end
 Msg("// LOADING SERVER WHITELIST ADDON \\\n")
 
 include("whitelist/core.lua")
-include("whitelist/chatcmd.lua")
 include("whitelist/config.lua")
+include("whitelist/chatcmd.lua")
+include("whitelist/saveload.lua")
+include("whitelist/joinauth.lua")
 
-Msg("// LOADED \\\n")
+Msg("        // LOADED \\\n")
