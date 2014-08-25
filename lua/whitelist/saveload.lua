3f49b4c8a08dbbf1b32a5af0947226e1882e7aa0
Ripmax <siranderson66@gmail.com>
2014-08-26 00:06:38 +0100

    MySQL

--- lua/whitelist/saveload.lua
+++ lua/whitelist/saveload.lua
@@ -1,14 +1,35 @@
 local config = Whitelist.Config
 local config_sql = config["mysql"]
 local useSQL = config_sql["enable"]
+local connected = false
 
-require("mysqloo")
+function Whitelist:ConnectToDB()
+	Whitelist.DB = {}
+	require("mysqloo")
+	if(!mysqloo) then Error("MySQLOO Failed to load!") end
 
-if(!mysqloo) then Error("MySQLOO Failed to load!") end
+	local ip = string.Explode(config_sql["host"],":")
 
-local ip = string.Explode(config_sql["host"],":")
+	local ip = ip[1]
+	local port = tonumber(ip[2])
 
-local ip = ip[1]
-local port = tonumber(ip[2])
-
-local __db = mysqloo.connect(ip,config_sql["user"],config_sql["password"],config_sql["database"],port)
+	Whitelist.DB.Connection = mysqloo.connect(ip,config_sql["user"],config_sql["password"],config_sql["database"],port)
+	
+	local dataB = Whitelist.DB.Connection
+	
+	function dataB:onConnected()
+		Msg("Successfully connected to database " .. config_sql["host"] .. "\n")
+		
+		local qy = self:query("SELECT * FROM whitelist")
+		function qy:onSuccess(ret)
+			Msg("Successfully polled database.\n")
+			PrintTable(ret) // debugging
+		end
+		
+		function qy:onError(error, sql)
+			// debugging
+			Msg("Error in query\nQuery: " .. sql .. "\nError: " .. error)
+		end
+		
+		qy:start()
+end
